01a236dfa9bfd6a566f51bd24d48245bbb3a929c
AlexShch446 <446.54.53@gmail.com>
2025-02-17 11:18:15 +0300

    minor update for dummy server, put port in env

--- dummy_http_server.py
+++ dummy_http_server.py
@@ -2,23 +2,22 @@
 This server is here only to satisfy Render's requirements for free deployment
 It is NOT needed for bot to function
 """
-
+import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b'Bot is running!')
 
-def start_http_server(port=8080):
+def start_http_server(port):
     server = HTTPServer(('0.0.0.0', port), SimpleHandler)
     print(f"HTTP server running on port {port}")
     server.serve_forever()
 
 def run_http_server():
-    http_thread = threading.Thread(target=start_http_server)
+    port = int(os.getenv('PORT', 8080))
+    http_thread = threading.Thread(target=start_http_server, args=(port,))
     http_thread.daemon = True
     http_thread.start()
